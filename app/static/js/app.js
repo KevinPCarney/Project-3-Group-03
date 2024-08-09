23a409c09abf7691dea9ba7d7b17c118ba2f24ec
kjrebeck <kjr7@pct.edu>
2024-08-09 17:36:30 -0400

    Upload app.js chart basics

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -29,6 +29,116 @@ function init() {
       buildMetadata(firstSample);
     });
 }
+// function to build both charts
+function buildCharts(sample) {
+  d3.json("/api/v1.0/get_dashboard/<nationality>").then((data) => {
+
+    // Get the samples field
+    let nationality_data = data.Nationality; 
+
+    // Filter the samples for the object with the desired sample number
+    let sample_info = sample_data.filter(x => x.id === sample)[0];
+    console.log(sample_info);
+
+    // Get the otu_ids, otu_labels, and sample_values
+    let otu_ids = sample_info.otu_ids;
+    let otu_labels = sample_info.otu_labels;
+    let sample_values = sample_info.sample_values;
+
+    // Build a Bubble Chart
+    let bubble_trace = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        color: otu_ids,
+        size: sample_values,
+        colorscale: 'YlOrRd',
+        type: 'heatmap'
+      }
+    }
+
+    // Render the Bubble Chart
+    // Create data array
+    let bubble_traces = [bubble_trace];
+
+    // Bubble Chart Layout
+    let bubble_layout = {
+      //background color, code from chat gbt
+      paper_bgcolor: 'rgba(47, 47, 47, 0.8)', // dark gray
+      plot_bgcolor: 'rgba(47, 47, 47, 0.8)', // dark gray
+      title: {
+        text: 'Bacteria Cultures Per Sample',
+        font: {
+          color: 'white',
+          size: 28
+        }},
+      xaxis: {
+        title: 'OTU ID',
+        color: "white"
+        },
+      yaxis: {
+        title: 'Number of Bacteria',
+        color: "white"
+        },
+      showlegend: false
+    }
+
+    // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("bubble", bubble_traces, bubble_layout);
+
+    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
+    let y_bar_val = otu_ids.map(x => `OTU: ${x}`);
+    //console log
+    console.log(y_bar_val);
+
+    // Build a Bar Chart
+    // slice and reverse the input 
+    let bar_trace = {
+      x: sample_values.slice(0,10).reverse(),
+      y: y_bar_val.slice(0,10).reverse(),
+      type: 'bar',
+      marker: {
+        colorscale: 'YlOrRd',
+        color: sample_values.slice(0,10).reverse()
+      },
+      text: otu_labels.slice(0,10).reverse(),
+      orientation: 'h'
+    }
+
+    // Render the Bar Chart
+    // Create data array
+    let bar_traces = [bar_trace];
+
+    // Bar Chart Layout
+    // Apply a title to the layout
+    let bar_layout = {
+      //background color, code from chat gbt
+      paper_bgcolor: 'rgba(47, 47, 47, 0.8)', // dark gray
+      plot_bgcolor: 'rgba(47, 47, 47, 0.8)', // dark gray
+      font: {
+          color: 'white'
+        },
+      title: {
+        text: "Top 10 Bacteria Cultures Found",
+        font: {
+          color: 'white',
+          size: 28
+        }},
+      xaxis: {
+        title: 'Number of Bacteria'
+        }
+    }
+    
+    // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("bar", bar_traces, bar_layout);
+    });
+}
+
+
+
+
 
 function createMap(){
   
