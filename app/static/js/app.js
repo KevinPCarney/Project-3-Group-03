4275999b8702a5e8b3f2b0d41d64ebc265649e61
martabaker <marta.baker97@gmail.com>
2024-08-12 19:05:46 -0400

    Got bubble chart to work!

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,19 +1,24 @@
 // Function to create bubble charts
-function buildBubble(newData) {
+function buildBubble(data) {
   // get user input
-  let nationality = d3.select("#selNationality");
+  // let nationality = d3.select("#selNationality").property("value");
 
     // Filter the samples for the object with the desired sample number
     //THIS FIELD
-    console.log(newData)
-    let driver_info = newData.filter(x => x.nationality === nationality);
-    console.log(driver_info);
+    // console.log("**")
+    // console.log(d3.map(data.bubble_data).values())
+    // console.log("**")
+    // let driver_info = newData.map(x => x.nationality);
+    // console.log(driver_info);
 
 
     // Get the otu_ids, otu_labels, and sample_values
-    let drivers_name = driver_info.last_name;
-    let avg_finish = driver_info.avg_finish;
-    let number_races = driver_info.number_races;
+    let drivers_last = data.bubble_data.map(row => row.last_name);
+    let drivers_first = data.bubble_data.map(row => row.first_name);
+    let avg_finish = data.bubble_data.map(row => row.avg_finish);
+    let number_races = data.bubble_data.map(row => row.number_races);
+
+    let drivers_name = `${drivers_first} ${drivers_last}`
 
    // Build a Bubble Chart
    let bubble_trace = {
@@ -56,61 +61,61 @@ function buildBubble(newData) {
   Plotly.newPlot("bubble", bubble_traces, bubble_layout);
 };
 
-// function to create a horizontal bar chart
-function buildBar(data) {
+// // function to create a horizontal bar chart
+// function buildBar(data) {
 
-    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-    let winner_info = data.map(x => x.nationality);
-    //console log
-    console.log(winner_info);
+//     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
+//     let winner_info = data.map(x => x.nationality);
+//     //console log
+//     console.log(winner_info);
 
-    // Build a Bar Chart
-    // slice and reverse the input 
-    // Horizontal bar chart where the y is Drivers Name and x is the number of 1st place finishes
-    let driversName = `${winner_info.first_name} ${winner_info.last_name}`
+//     // Build a Bar Chart
+//     // slice and reverse the input 
+//     // Horizontal bar chart where the y is Drivers Name and x is the number of 1st place finishes
+//     let driversName = `${winner_info.first_name} ${winner_info.last_name}`
 
-    let bar_trace = {
-      x: winNumber.slice(0,10).reverse(),
-      y: driversName.slice(0,10).reverse(),
-      type: 'bar',
-      marker: {
-        colorscale: 'YlOrRd',
-        color: sample_values.slice(0,10).reverse()
-      },
-      text: driversName.slice(0,10).reverse(),
-      orientation: 'h'
-    }
+//     let bar_trace = {
+//       x: winNumber.slice(0,10).reverse(),
+//       y: driversName.slice(0,10).reverse(),
+//       type: 'bar',
+//       marker: {
+//         colorscale: 'YlOrRd',
+//         color: sample_values.slice(0,10).reverse()
+//       },
+//       text: driversName.slice(0,10).reverse(),
+//       orientation: 'h'
+//     }
 
-    // Render the Bar Chart
-    // Create data array
-    let bar_traces = [bar_trace];
+//     // Render the Bar Chart
+//     // Create data array
+//     let bar_traces = [bar_trace];
 
-    // Bar Chart Layout
-    // Apply a title to the layout
-    let bar_layout = {
-      //background color, code from chat gbt
-      // paper_bgcolor: 'rgba(47, 47, 47, 0.8)', // dark gray
-      // plot_bgcolor: 'rgba(47, 47, 47, 0.8)', // dark gray
-      font: {
-          color: 'white'
-        },
-      title: {
-        text: "Driver Wins by Nationality",
-        font: {
-          color: 'white',
-          size: 28
-        }},
-      xaxis: {
-        title: 'Driver'
-        }
-    }
+//     // Bar Chart Layout
+//     // Apply a title to the layout
+//     let bar_layout = {
+//       //background color, code from chat gbt
+//       // paper_bgcolor: 'rgba(47, 47, 47, 0.8)', // dark gray
+//       // plot_bgcolor: 'rgba(47, 47, 47, 0.8)', // dark gray
+//       font: {
+//           color: 'white'
+//         },
+//       title: {
+//         text: "Driver Wins by Nationality",
+//         font: {
+//           color: 'white',
+//           size: 28
+//         }},
+//       xaxis: {
+//         title: 'Driver'
+//         }
+//     }
     
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("bar", bar_traces, bar_layout);
-  }
+//     // Render the plot to the div tag with id "plot"
+//     Plotly.newPlot("bar", bar_traces, bar_layout);
+//   }
 
 // function to run on page load
-function init_dashboard() {
+function init_dropdown() {
   // might add filters 
     d3.json("/api/v1.0/get_dropdown").then((data) => {
 
@@ -134,18 +139,16 @@ function init_dashboard() {
       }
   
       // Get the first nationality from the list
-      let allNationality = names;
+      let allNationality = names[0];
   
       // Build charts and metadata panel with the first nationality
-      buildBar(allNationality);
+      // buildBar(allNationality);
       buildBubble(allNationality);
       // buildMetadata(firstSample);
     });
 }
 
-
-// Function for event listener
-function optionChanged(newNationality) {
+function init_dashboard(){
   // get user input
   let nationality = d3.select("#selNationality").property("value");
 
@@ -154,11 +157,27 @@ function optionChanged(newNationality) {
   d3.json(url_dash).then(function(data){
 
     // Build charts and metadata panel each time a new sample is selected
-    buildBar(data.bar_data);
-    buildBubble(data.bubble_data);
-    buildMetadata(newNationality);
+    // buildBar(data.bar_data);
+    buildBubble(data);
   })
+
+
+}
+
+// event listener
+function optionChanged() {
+  let nationality = d3.select("#selNationality");
+
+  let url_dash = `/api/v1.0/get_dashboard/${nationality}`
+
+  d3.json(url_dash).then(function(data){
+
+    // Build charts and metadata panel each time a new sample is selected
+    init_dashboard(data)
+  })
+
 }
 
 // Initialize the dashboard
 init_dashboard();
+init_dropdown()
